3985ba60935455a25f35edb44ff7507aa865539c
AngadSudan <angadsudan453@gmail.com>
2025-10-27 13:11:13 +0530

    [BACKEND | UPDATED SERVER] - updated builder server env

--- apps/builder-server/index.ts
+++ apps/builder-server/index.ts
@@ -13,7 +13,7 @@ app.use(
   cors({
     origin: [
       process.env.FRONTEND_URL || "http://localhost:5173",
-      "http://localhost:8000",
+      process.env.BACKEND_URL || "http://localhost:8000",
       "http://localhost:3000",
     ],
     credentials: true,
@@ -26,7 +26,7 @@ app.use(
       "Origin",
       "Accept",
     ],
-  }),
+  })
 );
 app.use(express.json());
 
@@ -58,7 +58,7 @@ app.post("/deploy", async (req, res) => {
         PROJECT_NAME: dbProject?.name.replaceAll(" ", "-").replaceAll(",", "-"),
         projectId,
       }),
-    ],
+    ]
   );
   res.json({
     message: "Getting the deployment ready for you",
@@ -177,7 +177,7 @@ async function runDockerBuild(data: {
 client.consumeMessageViaConsumer(
   "deployment-consumer",
   "pending-docker-build",
-  runDockerBuild,
+  runDockerBuild
 );
 
 app.use(
@@ -185,12 +185,12 @@ app.use(
     err: any,
     req: express.Request,
     res: express.Response,
-    next: express.NextFunction,
+    next: express.NextFunction
   ) => {
     console.log("this is from global error handlers");
     console.error(err.stack || err.message || err);
     next();
-  },
+  }
 );
 
 app
